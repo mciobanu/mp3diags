084e7954196934f85c4eea322526dde1e79f2f7c
Marian Ciobanu <ciobi07@users.sourceforge.net>
2011-07-31 10:26:38 +0300

    Made MP3DiagsCLI.cmd binary and removed MP3DiagsCLI-unstable.cmd

--- MP3DiagsCLI-unstable.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem this file is overwritten by the NSIS script, using the full dir for the exe
-
-MP3DiagsWindows-unstable.exe %* > %TEMP%\Mp3DiagsOut.txt
-type %TEMP%\Mp3DiagsOut.txt
-del %TEMP%\Mp3DiagsOut.txt
--- MP3DiagsCLI.cmd
+++ MP3DiagsCLI.cmd
@@ -1,4 +1,6 @@
 @echo off
+rem this file is overwritten by the NSIS script, using the full dir for the exe
+
 MP3DiagsWindows.exe %* > %TEMP%\Mp3DiagsOut.txt
 type %TEMP%\Mp3DiagsOut.txt
 del %TEMP%\Mp3DiagsOut.txt

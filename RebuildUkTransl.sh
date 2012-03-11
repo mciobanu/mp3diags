4c96674967a43c3d7fb2012a0f722d1ab4a67605
Marian Ciobanu <ciobi07@users.sourceforge.net>
2012-03-11 19:03:55 +0200

    now removing obsolete translations

--- RebuildUkTransl.sh
+++ RebuildUkTransl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rm src/translations/mp3diags_en_UK.ts
-lupdate src/src.pro 2>&1 | grep -v '^/usr/'
+#lupdate src/src.pro 2>&1 | grep -v '^/usr/'
+lupdate -no-obsolete src/src.pro 2>&1 | grep -v '^/usr/'
 echo -e '\n\n'
 src/translations/autoTransl/MixedCase src/translations/mp3diags_en_UK.ts
 mv -f /tmp/MiXeDcAsE src/translations/mp3diags_en_UK.ts

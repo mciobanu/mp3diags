656a742904418a31e0e148e103f5ef1355bd19d4
Marian Ciobanu <ciobi07@users.sourceforge.net>
2012-12-02 06:52:53 +0200

    Fedora support for translation

--- MakeTranslations.sh
+++ MakeTranslations.sh
@@ -8,9 +8,16 @@ if [[ "$#" != "0" ]]; then
     exit
 fi
 
+LRelease=lrelease
+
+if [ -f /etc/fedora-release ] ; then
+    LRelease=lrelease-qt4
+fi
+
+
 #lupdate mp3diags.pro
 #lrelease src/src.pro
-lrelease src/translations/mp3diags_*.ts
+$LRelease src/translations/mp3diags_*.ts
 
 if [[ "$?" != "0" ]] ; then
     echo -e "\nThere was an error trying to build the translations. This shouldn't impact other parts of the build process.\n" >&2

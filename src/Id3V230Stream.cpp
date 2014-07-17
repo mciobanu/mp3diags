3756b96f27ddea210e03e612635fd4bdd48c6769
Marian Ciobanu <ciobi07@users.sourceforge.net>
2014-07-17 10:56:17 +0300

    Removed incorrect assert

--- src/Id3V230Stream.cpp
+++ src/Id3V230Stream.cpp
@@ -632,7 +632,6 @@ void Id3V230StreamWriter::addBinaryFrame(const std::string& strName, vector<char
 void Id3V230StreamWriter::addImg(std::vector<char>& vcData)
 {
     int n (cSize(vcData));
-    CB_ASSERT1 (n > 100, m_strDebugFileName);
     char* p (&vcData[0]);
     CB_ASSERT1 (0 == *p || 3 == *p, m_strDebugFileName); // text encoding // this should be kept in synch with Id3V2StreamBase::decodeApic() //ttt1 triggered according to mail; might have been caused by SmallerImageRemover::apply() incorrectly assuming that an invalid APIC frame is a large picture; the test using the frame name was replaced after the assert with a test using m_eApicStatus; will have to wait until some MP3 is received that triggered this to be sure
 

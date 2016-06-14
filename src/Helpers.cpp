a0487e62fa29f7c72fc37671b7e08fcd618fce54
Marian Ciobanu <ciobi07@users.sourceforge.net>
2016-06-14 18:45:00 +0300

    OS/2 fix

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -1739,7 +1739,26 @@ LastStepTracer::~LastStepTracer()
 //=============================================================================================
 //=============================================================================================
 
+// Patch by Elbert Pol, to make the linker work on OS/2. Not sure why adding "LIBS += -lrt" to src.pro didn't seem to work.
+# if defined OS2
+inline int
+clock_gettime(int clock_id, struct timespec *ts)
+{
+     struct timeval tv;
 
+#if 0
+     if (clock_id != CLOCK_REALTIME) {
+         errno = EINVAL;
+         return (-1);
+     }
+#endif
+     if (gettimeofday(&tv, NULL) < 0)
+         return (-1);
+     ts->tv_sec = tv.tv_sec;
+     ts->tv_nsec = tv.tv_usec * 1000;
+     return (0);
+}
+#endif
 
 int64_t CB_LIB_CALL Timer::getCrtTime() const // returns time in nanoseconds
 {

9b305f5c8dd1fdf0c80c0539b84ec5e9e607e678
Marian Ciobanu <ciobi07@users.sourceforge.net>
2016-07-04 23:07:46 +0300

    Fix for automatic conversion of streams to bool

--- src/MainFormDlgImpl.cpp
+++ src/MainFormDlgImpl.cpp
@@ -119,7 +119,7 @@ namespace
         {
             m_strFileName = strFileName;
             ofstream_utf8 out (m_strFileName.c_str(), ios_base::app);
-            return out;
+            return (bool)out;
         }
 
         void close() { m_strFileName.clear(); }
@@ -128,7 +128,7 @@ namespace
         {
             ofstream_utf8 out (m_strFileName.c_str(), ios_base::app);
             out << s;
-            return out;
+            return (bool)out;
         }
     };
 #else // #ifndef WIN32
